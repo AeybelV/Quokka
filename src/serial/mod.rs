280a992719c01fe6fb79703c08d972c01b8b7948
Aeybel Varghese <aeybelvarghese@gmail.com>
2025-04-20 22:23:32 -0600

    Defined an API for the SerialDevice subsystem
    Drivers for serial devices can be created by imlementing the
    SerialDevice trait. This provides a common API for serial devices to
    initialize, and perform byte wise read/writes.

--- src/serial/mod.rs
+++ src/serial/mod.rs
@@ -7,6 +7,27 @@ pub mod serial;
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+/// Serial Device Trait
+/// All Serial Device Drivers implement this trait
+#[cfg(feature = "serialdevice")]
+pub trait SerialDevice {
+    /// Initializes the device
+    fn init(&self);
+
+    /// Transmits a byte
+    ///
+    /// * `byte`: byte to transmit
+    fn write_byte(&self, byte: u8);
+
+    /// Reads a byte from the device
+    fn read_byte(&self) -> Option<u8>;
+
+    /// Enables the device
+    fn enable(&self);
+
+    /// Disables the device
+    fn disable(&self);
+}
 
 #[cfg(feature = "pl011")]
 pub mod amba_pl011;
